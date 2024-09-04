325af3cc18159f686038157b56d77aab2b05d547
AdityaKumarPrajapati <aditya.prajapati@successive.tech>
2024-09-04 19:51:02 +0530

    architecture code

--- src/components/HomePageBanner.js
+++ src/components/HomePageBanner.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import bannerImage from '../asset/images/bnr_img.png'
 import './css/Home.css'; // Import CSS for styling the Home component
+import ZipCodeStarted from './ZipCodeStarted';
 
 function HomePageBanner() {
   return (
@@ -11,9 +12,14 @@ function HomePageBanner() {
         <img src={bannerImage} alt="Banner" />
       </div>
       <div className='bannerContent'>
-        <p class="title-white title-banner homePageBanner">Award-winning Chefs
-          Health Driven Menus
-          Delivered Fresh Daily</p>
+        <div>
+          <p class="title-white title-banner homePageBanner">
+            Award-winning Chefs
+            Health Driven Menus
+            Delivered Fresh Daily
+          </p>
+          <ZipCodeStarted />
+        </div>
       </div>
     </div>
   );
