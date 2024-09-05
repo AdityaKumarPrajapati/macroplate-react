07aba75c1b58efe2da57ca5918c9438017941f45
Diptesh Kumar <diptesh.kumar@successive.tech>
2024-09-05 17:32:31 +0530

    changes pur page

---
+++ src/dataContentObjects/ourPageDataContent.js
@@ -0,0 +1,28 @@
+const OurApproachContetntData = {
+    'SustainableData' : {
+        'imageTitle' : 'Sustainable Sourcing',
+        'imageClassName' : 'SustainableImage',
+        'leftText' : 'At the core of what we do is providing you with delicious, nutritious meals that not only nourish your body but also respect our environment.',
+        'rightText' : 'Our recipes are hand-picked to be non-inflammatory and optimize overall health while supporting a wide range of diets. We take care in sourcing all animal proteins from farms where no antibiotics or hormones were used, and seafood comes from sustainable sources that protect marine life.'
+    },
+    'ReducingData' : {
+        'imageTitle' : 'Reducing Waste',
+        'imageClassName' : 'ReducingImage',
+        'leftText' : 'We plan and order our ingredients before meals are produced to keep our food waste below 1%.',
+        'rightText' : 'The US food supply is being squandered, with an estimated 30-40% of it ending up in landfills. Wholesome food that could have helped feed families in need is sent to landfills. By signing up for meal programs in advance, clients help us to minimize the number of resources utilized when it comes time to create their delicious dishes.'
+    },
+    'LeaveNoTraceData' : {
+        'imageTitle' : 'Leaving No Trace',
+        'imageClassName' : 'LeaveNoTraceImage',
+        'leftText' : 'We commit to removing an equivalent amount of plastic from nature as is used in our packaging!',
+        'rightText' : 'We’ve teamed up with rePurpose Global to fight plastic pollution and make a real difference in our environment. Together, lets create harmony between us and the planet.'
+    },
+    'OffsettingEmmissionsData' : {
+        'imageTitle' : 'Offsetting Emmissions',
+        'imageClassName' : 'OffsettingEmmissionsImage',
+        'leftText' : 'With an eye toward the future, weve partnered with The Conservation Fund to help ensure a healthier planet for generations ahead.',
+        'rightText' : 'By planting trees across our country, we can offset our carbon footprint and give back to mother nature!'
+    },
+}
+
+export default OurApproachContetntData;
