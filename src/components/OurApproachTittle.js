e37494611a5752e4b170622e7162005759a32ab5
Diptesh Kumar <diptesh.kumar@successive.tech>
2024-09-05 15:53:47 +0530

    our page

---
+++ src/components/OurApproachTittle.js
@@ -0,0 +1,13 @@
+function OurApproachTittle() {
+    return (
+        <div className="ourApproachTittle">
+            <h2 className="marginZero paddingZero ourApproachHeading">Our Approach</h2>
+            <div className="ourApproachDescription marginZero paddingZero">
+                <p className="marginZero paddingZero">At Macroplate, we recognize the secret superpower in food:</p>
+                <p className="marginZero paddingZero">its ability to nourish us holistically and become our own medicine. Nature is our most reliable ally regarding mindfull wellness - let's embrace her gifts!</p>
+            </div>
+        </div>
+    );
+}
+
+export default OurApproachTittle;
